6e04906c87a561acd1ec5a16956a71f7c52a79be
vivek sharma <45270024+vvksharrma@users.noreply.github.com>
2024-06-27 11:30:09 +0530

    Update Arrays

---
+++ Arrays/removeDuplicate.js
@@ -0,0 +1,20 @@
+//Remove duplicate item from array 
+const arr = [1,2,3,4,1,2];
+const b=[];
+for(let i=0;i<arr.length;i++){
+if(b.indexOf(arr[i]) == -1){
+b.push(arr[i])
+}
+}
+console.log("removed array value",b)
+
+
+//second method using filter
+const arr = [1,2,3,4,1,2];
+const b=[];
+arr.filter((dup)=>{
+if(b.indexOf(arr[dup]) == -1){
+b.push(arr[dup])
+}
+})
+console.log("removed array value",b)
