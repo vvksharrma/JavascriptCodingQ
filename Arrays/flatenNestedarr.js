095b6e6ccb8a7fe7e7faea3273e09c701c1bf749
vivek sharma <45270024+vvksharrma@users.noreply.github.com>
2026-01-08 12:50:01 +0530

    Update flatenNestedarr.js

--- Arrays/flatenNestedarr.js
+++ Arrays/flatenNestedarr.js
@@ -4,3 +4,17 @@ const flattenArray = (nestedArray) => {
   
   }; 
   console.log(flattenArray([1,[3,3,[5,1]]]))
+
+
+//coding implementation 
+
+const flatten(arr){
+ return are.reduce((acc,val){
+
+
+if(Array.isArray(val){
+return acc.concat(flatten(val))
+}
+return acc.concat(val)
+},[])
+}
